6949bcb7c9dda2da17b4561920b9594af6471bef
Colin Bowes <59944164+OMHavec@users.noreply.github.com>
2021-10-19 19:48:10 -0400

    shopping page first commit

--- housemates/resources/views/pages/shopping.blade.php
+++ housemates/resources/views/pages/shopping.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.layout')
 @section('content')
 
-<h1>Shopping</h1>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Colin Bowes">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Housemates</title>
+        <link href="" rel="stylesheet">
+    </head>
+    <body>
+        <nav>
+            <h1>Home</h1>
+            <h1>Calendar</h1>
+            <h1>Shopping</h1>
+            <h1>Chat</h1>
+            <h1>Contact</h1>
+            <h1>Settings</h1>
+        </nav>
+    </body>
+</html>
 
 @endsection
