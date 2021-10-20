3448c0dcfec6936c76bae8d0741bf78d54d85adc
Colin Bowes <59944164+OMHavec@users.noreply.github.com>
2021-10-19 20:12:48 -0400

    Created base table

--- housemates/resources/views/pages/shopping.blade.php
+++ housemates/resources/views/pages/shopping.blade.php
@@ -1,6 +1,87 @@
 @extends('layouts.layout')
 @section('content')
 
-<h1>Shopping</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Done?</th>
+            <th>Item</th>
+            <th>Urgency</th>
+            <th>Assigned</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+            <td>
+                <div contenteditable></div>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
 @endsection
