c79fb5136133071c06d345553f5accf5e4fcc17e
Colin Bowes <59944164+OMHavec@users.noreply.github.com>
2021-11-07 17:41:50 -0500

    Updated header.css, shopping.blade.php, layout.blade.php

--- housemates/resources/views/layouts/layout.blade.php
+++ housemates/resources/views/layouts/layout.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <link href="/css/header.css" type="text/css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <title>Housemates</title>
     </head>
     <body class="">
--- housemates/resources/views/pages/shopping.blade.php
+++ housemates/resources/views/pages/shopping.blade.php
@@ -13,62 +13,122 @@
     </thead>
     <tbody>
         <tr>
-            <td><input type="checkbox" name="row1"/>&nbsp;</td>
-            <td>
+            <td class="checkbox-column"><input type="checkbox" name="row1"/>&nbsp;</td>
+            <td class="item-column">
                 <div contenteditable></div>
             </td>
-            <td>
+            <td class="urgency-column">
                 <div contenteditable></div>
             </td>
-            <td>
-                <div contenteditable></div>
-            </td>
-        </tr>
-        <tr>
-            <td><input type="checkbox" name="row2"/>&nbsp;</td>
-            <td>
-                <div contenteditable></div>
-            </td>
-            <td>
-                <div contenteditable></div>
-            </td>
-            <td>
+            <td class="assignee-column">
                 <div contenteditable></div>
             </td>
         </tr>
         <tr>
-            <td><input type="checkbox" name="row3"/>&nbsp;</td>
-            <td>
+            <td class="checkbox-column"><input type="checkbox" name="row2"/>&nbsp;</td>
+            <td class="item-column">
                 <div contenteditable></div>
             </td>
-            <td>
+            <td class="urgency-column">
                 <div contenteditable></div>
             </td>
-            <td>
-                <div contenteditable></div>
-            </td>
-        </tr>
-        <tr>
-            <td><input type="checkbox" name="row4"/>&nbsp;</td>
-            <td>
-                <div contenteditable></div>
-            </td>
-            <td>
-                <div contenteditable></div>
-            </td>
-            <td>
+            <td class="assignee-column">
                 <div contenteditable></div>
             </td>
         </tr>
         <tr>
-            <td><input type="checkbox" name="row5"/>&nbsp;</td>
-            <td>
+            <td class="checkbox-column"><input type="checkbox" name="row3"/>&nbsp;</td>
+            <td class="item-column">
                 <div contenteditable></div>
             </td>
-            <td>
+            <td class="urgency-column">
                 <div contenteditable></div>
             </td>
-            <td>
+            <td class="assignee-column">
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td class="checkbox-column"><input type="checkbox" name="row4"/>&nbsp;</td>
+            <td class="item-column">
+                <div contenteditable></div>
+            </td>
+            <td class="urgency-column">
+                <div contenteditable></div>
+            </td>
+            <td class="assignee-column">
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td class="checkbox-column"><input type="checkbox" name="row5"/>&nbsp;</td>
+            <td class="item-column">
+                <div contenteditable></div>
+            </td>
+            <td class="urgency-column">
+                <div contenteditable></div>
+            </td>
+            <td class="assignee-column">
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td class="checkbox-column"><input type="checkbox" name="row6"/>&nbsp;</td>
+            <td class="item-column">
+                <div contenteditable></div>
+            </td>
+            <td class="urgency-column">
+                <div contenteditable></div>
+            </td>
+            <td class="assignee-column">
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td class="checkbox-column"><input type="checkbox" name="row7"/>&nbsp;</td>
+            <td class="item-column">
+                <div contenteditable></div>
+            </td>
+            <td class="urgency-column">
+                <div contenteditable></div>
+            </td>
+            <td class="assignee-column">
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td class="checkbox-column"><input type="checkbox" name="row8"/>&nbsp;</td>
+            <td class="item-column">
+                <div contenteditable></div>
+            </td>
+            <td class="urgency-column">
+                <div contenteditable></div>
+            </td>
+            <td class="assignee-column">
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td class="checkbox-column"><input type="checkbox" name="row9"/>&nbsp;</td>
+            <td class="item-column">
+                <div contenteditable></div>
+            </td>
+            <td class="urgency-column">
+                <div contenteditable></div>
+            </td>
+            <td class="assignee-column">
+                <div contenteditable></div>
+            </td>
+        </tr>
+        <tr>
+            <td class="checkbox-column"><input type="checkbox" name="row10"/>&nbsp;</td>
+            <td class="item-column">
+                <div contenteditable></div>
+            </td>
+            <td class="urgency-column">
+                <div contenteditable></div>
+            </td>
+            <td class="assignee-column">
                 <div contenteditable></div>
             </td>
         </tr>
