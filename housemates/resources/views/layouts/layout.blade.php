c31479c965393e5510446d327ca61bd5df81178d
Nicolas-A-Becker <nico_becker1@aol.com>
2021-11-07 15:34:58 -0500

    Designed contacts page
    Currently all css is within layout page. To be moved later

--- housemates/resources/views/layouts/layout.blade.php
+++ housemates/resources/views/layouts/layout.blade.php
@@ -4,6 +4,101 @@
         <meta charset="utf-8">
 
         <title>HouseMates</title>
+<<<<<<< Updated upstream
+=======
+        <h>HouseMates</h>
+
+        <style>
+			body {
+				text-align: left;
+				background-color: #E6ECEF;
+				background-size: cover;
+				background-position: center;
+				color: white;
+				font-family: Roboto;
+                margin: 0;
+			}
+            #nav-rectangle {
+                width: 100%;
+                height: 96px;
+                background: #FFFFFF;
+                position:absolute;
+                top: 70px;
+            }
+            a {
+                text-decoration: none;
+                color: black;
+                font-family: Roboto;
+            }
+            a:hover {
+                color: #698384;
+            }
+            nav {
+                width: 100vw;
+                background-color: #FFFFF;
+                position: absolute;
+                top: 105px;
+            }
+            ul {
+                margin: 0;
+                padding: 0;
+                display: flex;
+            }
+            li {
+                list-style-type: none;
+                margin: 0 6vw;
+                font-size: 3vh;
+            }
+            h1 {
+                color: #698384;
+                text-align: center;
+
+            }
+            .office{
+                position: relative;
+                left: 25%;
+                background: white;
+                width: 50%;
+                height: 160px;
+                border-radius: 15px;
+                margin-top: 4%;
+            }
+            .office h2{
+                text-align: center;
+                padding-top: 10px;
+                color: black;
+                font-size: 34px;
+            }
+            .office p{
+                color: black;
+                text-align: center;
+                font-size: 18px;
+            }
+            .roommates{
+                background: white;
+                height: 500px;
+                margin-top: 2%;
+                display: flex;
+            }
+            .rm{
+                background: #E6ECEF;
+                margin-left: 1%;
+                margin-right: 1%;
+                margin-top: 2%;
+                padding: 1%;
+                height: 300px;
+                flex: 1;
+                border-radius: 15px;
+                color: black;
+            }
+            .rm h3{
+                text-align: center
+            }
+            .rm p{
+                padding: 1%;
+            }
+        </style>    
+>>>>>>> Stashed changes
     </head>
     <body class="">
         @yield('content')
--- housemates/resources/views/pages/contact.blade.php
+++ housemates/resources/views/pages/contact.blade.php
@@ -4,7 +4,39 @@
 <h1>Contact</h1>
 <div class="office">
     <h2>Office-Info</h2>
-    <p></p>
+    
+    <p>Phone: (***) ***-****</p>
+    <p>Address: 123 Fake Ave, Columbia, SC</p>
 </div>
-<div class="roomie"></div>
+<div class="roommates">
+    <div class="rm">
+        <h3>RM #1</h3>
+        <p>Phone:</p>
+        <p>Emergencry Phone:</p>
+        <p>Instagram</p>
+        <p>Snapchat</p>
+        <p>Venmo</p>
+    </div>
+    <div class="rm">
+        <h3>RM #2</h3>
+        <p>Phone:</p>
+        <p>Emergencry Phone:</p>
+        <p>Instagram</p>
+        <p>Snapchat</p>
+        <p>Venmo</p>    </div>
+    <div class="rm">
+        <h3>RM #3</h3>
+        <p>Phone:</p>
+        <p>Emergencry Phone:</p>
+        <p>Instagram</p>
+        <p>Snapchat</p>
+        <p>Venmo</p>    </div>
+    <div class="rm">
+        <h3>RM #4</h3>
+        <p>Phone:</p>
+        <p>Emergencry Phone:</p>
+        <p>Instagram</p>
+        <p>Snapchat</p>
+        <p>Venmo</p>    </div>
+</div>
 @endsection
