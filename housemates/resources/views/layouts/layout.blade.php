ba024f38ba46f45b47247f58cd085ff1c5820d1b
Nicolas-A-Becker <nico_becker1@aol.com>
2021-11-11 14:34:06 -0500

    adding vendor I guess

--- housemates/resources/views/layouts/layout.blade.php
+++ housemates/resources/views/layouts/layout.blade.php
@@ -6,8 +6,47 @@
         <link href="/css/contact.css" type="text/css" rel="stylesheet">
         <title>HouseMates</title>
         <h>HouseMates</h>
+<<<<<<< HEAD
+=======
+
+>>>>>>> origin
     </head>
     <body class="">
+        <h1>HouseMates</h1>
+        <div id="nav-rectangle"></div>
+
+        <nav>
+        <ul>
+            <li>
+            <!-- link one -->
+            <a href="#Home"> Home </a>
+            </li>
+            <li>
+            <!-- link two -->
+            <a href="#Calendar"> Calendar </a>
+            </li>
+            <li>
+            <!-- link three -->
+            <a href="#Grocery"> Grocery </a>
+            </li>
+            <li>
+            <!-- link four -->
+            <a href="#Chat"> Chat </a>
+            </li>
+            <li>
+            <!-- link five -->
+            <a href="#Contacts"> Contacts </a>
+            </li>
+            <li>
+            <!-- link six -->
+            <a href="#Settings"> Settings </a>
+            </li>
+        </ul>
+        </nav>
+    <!-- put in navigation to other pages -->
+
+
+
         @yield('content')
     </body>
 </html>
