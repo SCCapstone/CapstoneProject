b84745009b0980c8295c4cf71ef2fbf3f18cf324
Lauren Lee <lel2@email.sc.edu>
2021-11-15 16:13:56 -0500

    added sign in page and all correlating layout/css pages

---
+++ housemates/resources/views/layouts/signInUp.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link href="/css/sign.css" type="text/css" rel="stylesheet">
+		<!-- <meta name="google-signin-client_id" content="224820970107-7dsle63ops8sblvn9aheitj99o0bmq9d.apps.googleusercontent.com.apps.googleusercontent.com"> -->
+	</head>
+
+	<body class="">
+		<h1>Welcome Roommates!</h1>
+	<div id="welcomebox"></div>
+	<div id="divider"></div>
+
+    @yield('content')
+
+	<!-- <div id="userandpass" style = "top: 360px; left: 525px; position: absolute;">Username:</div>
+	<div id="entry" style = "top: 400px; left: 525px;"></div>
+	<div id="userandpass" style = "top: 510px; left: 525px; position: absolute;">Password:</div>
+	<div id="entry" style = "top: 550px; left: 525px;"></div> -->
+
+	<!-- Sign in button goes here -->
+
+    <!-- <div class="g-signin2" data-onsuccess="onSignIn"></div>
+	<script>
+		function onSuccess(googleUser) {
+      			console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+		}
+    		function onFailure(error) {
+      			console.log(error);
+    		}
+    		function renderButton() {
+      			gapi.signin2.render('my-signin2', {
+        			'scope': 'profile email',
+        			'width': 240,
+        			'height': 50,
+        			'longtitle': true,
+        			'theme': 'dark',
+        			'onsuccess': onSuccess,
+        			'onfailure': onFailure
+      			});
+    		}
+	</script>
+	<script src="https://apis.google.com/js/platform.js" async defer></script> -->
+
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
+	<meta name="google-signin-client_id" content="capstoneproject-332219.apps.googleusercontent.com.apps.googleusercontent.com.apps.googleusercontent.com">
+	<div class="g-signin2" data-onsuccess="onSignIn" style = "top: 650px; left: 625px; position: absolute; height: 50px; width: 200px;"></div>
+	<script>
+		function onSuccess(googleUser) {
+      			console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+		}
+    		function onFailure(error) {
+      			console.log(error);
+    		}
+    		function renderButton() {
+      			gapi.signin2.render('my-signin2', {
+        			'scope': 'profile email',
+        			'width': 240,
+        			'height': 50,
+        			'longtitle': true,
+        			'theme': 'dark',
+        			'onsuccess': onSuccess,
+        			'onfailure': onFailure
+      			});
+    		}
+	</script>
+
+	<div id ="bottom">The home all-in-one-app! Share calendars, organize chores, decide on grocery lists, assign tasks, or discuss topics! We strive to help your household become a more transparent one.</bottom>
+	</body>
+
+</html>
--- housemates/resources/views/welcome.blade.php
+++ housemates/resources/views/welcome.blade.php
@@ -1,14 +1,29 @@
-@extends('layouts.layout')
+@extends('layouts.signInUp')
 @section('content')
 
-<h1>Welcome</h1>
-<a href="/pages/calendar">Calendar</a>
+<title>Sign In</title>
+<div id="sign">Sign In</div>
+
+<body>
+
+    <form>
+         	Username:<br> <input type="text" name="username" placeholder="Enter here">
+         	<br>
+		    <br>
+         	Password:<br> <input type="text" name="password" placeholder="Enter here">
+         	<br>
+    </form>
+
+    <a href="sign-up.blade.php">Don't have an account? Sign up here!</a>
+<!-- <a href="/pages/calendar">Calendar</a>
 <a href="/pages/chat">Chat</a>
 <a href="/pages/index">Index</a>
 <a href="/pages/settings">Settings</a>
 <a href="/pages/shopping">Shopping</a>
 <a href="/pages/sign-in">Sign-in</a>
 <a href="/pages/sign-up">Sign-up</a>
-<a href="/pages/contact">Contact</a>
+<a href="/pages/contact">Contact</a> -->
+
+</body>
 
 @endsection
