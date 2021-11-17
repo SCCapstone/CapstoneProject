595aa8aac792cbb2b9f5b18b05e3112330141404
Nicolas-A-Becker <nico_becker1@aol.com>
2021-11-16 20:04:43 -0500

    reformatted with <p> tags
    Used <br> and <p> tags to give an area for data to populate once there are users

--- housemates/resources/views/pages/contact.blade.php
+++ housemates/resources/views/pages/contact.blade.php
@@ -12,33 +12,35 @@
 <div class="roommates">
     <div class="rm">
         <h3>RM #1</h3>
-        <p>Phone:</p>
-        <p>Emergencry Phone:</p>
-        <p>Instagram</p>
-        <p>Snapchat</p>
-        <p>Venmo</p>
+        <p>Phone:</p><p id="rm1P"></p><br>
+        <p>Emergencry Phone:</p><p id="rm1EP"></p><br>
+        <p>Instagram</p><p id="rm1I"></p><br>
+        <p>Snapchat</p><p id="rm1S"></p><br>
+        <p>Venmo</p><p id="rm1V"></p><br>
     </div>
     <div class="rm">
         <h3>RM #2</h3>
-        <p>Phone:</p>
-        <p>Emergencry Phone:</p>
-        <p>Instagram</p>
-        <p>Snapchat</p>
-        <p>Venmo</p>
+        <p>Phone:</p><p id="rm2P"></p><br>
+        <p>Emergencry Phone:</p><p id="rm2EP"></p><br>
+        <p>Instagram</p><p id="rm2I"></p><br>
+        <p>Snapchat</p><p id="rm2S"></p><br>
+        <p>Venmo</p><p id="rm2V"></p><br>
     </div>
     <div class="rm">
         <h3>RM #3</h3>
-        <p>Phone:</p>
-        <p>Emergencry Phone:</p>
-        <p>Instagram</p>
-        <p>Snapchat</p>
-        <p>Venmo</p>    </div>
+        <p>Phone:</p><p id="rm3P"></p><br>
+        <p>Emergencry Phone:</p><p id="rm3EP"></p><br>
+        <p>Instagram</p><p id="rm3I"></p><br>
+        <p>Snapchat</p><p id="rm3S"></p><br>
+        <p>Venmo</p><p id="rm3V"></p><br>
+    </div>
     <div class="rm">
         <h3>RM #4</h3>
-        <p>Phone:</p>
-        <p>Emergencry Phone:</p>
-        <p>Instagram</p>
-        <p>Snapchat</p>
-        <p>Venmo</p>    </div>
+        <p>Phone:</p><p id="rm4P"></p><br>
+        <p>Emergencry Phone:</p><p id="rm4EP"></p><br>
+        <p>Instagram</p><p id="rm4I"></p><br>
+        <p>Snapchat</p><p id="rm4S"></p><br>
+        <p>Venmo</p><p id="rm4V"></p><br>
+    </div>
 </div>
 @endsection
