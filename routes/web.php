60d7c3a4457db9f97bcc3955333ad3b4c977a7ea
Colin Bowes <59944164+OMHavec@users.noreply.github.com>
2021-12-02 20:14:46 -0500

    Deleted old model and route

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,13 @@ Route::get('/', function () {
 //routes
 use App\Http\Controllers\HouseController;
 Route::get('/pages/contact', [HouseController::class, 'contact']);
-
 Route::get('/pages/settings', [HouseController::class, 'settings']);
-
 Route::get('/pages/sign-in', [HouseController::class, 'signin']);
 Route::get('/pages/sign-up', [HouseController::class, 'signup']);
 Route::get('/pages/home-page', [HouseController::class, 'homepage']);
-
 Route::get('/pages/shopping', [HouseController::class, 'shopping']);
 Route::get('/pages/calendar', [HouseController::class, 'calendar']);
 Route::get('/pages/chat', [HouseController::class, 'chat']);
-
 Route::get('/pages/index', [HouseController::class, 'index']);
 
 Route::get('/pages/settingsPages/emergencySettings', [HouseController::class, 'emergencySettings'])->name('settings.emergency');
@@ -39,10 +35,6 @@ Route::get('/pages/settingsPages/informationSettings', [HouseController::class, 
 Route::get('/pages/settingsPages/personalSettings', [HouseController::class, 'personalSettings'])->name('settings.personal');
 Route::get('/pages/settingsPages/roommatesSettings', [HouseController::class, 'roommatesSettings'])->name('settings.roommates');
 Route::get('/pages/settingsPages/socialsSettings', [HouseController::class, 'socialsSettings'])->name('settings.socials');
-
-Route::get('housedb', function() {
-    return view('housedb');
-});
 
 Route::post('/pages/settingsPages/emergencySettings', [HouseController::class, 'storeEmergencySettings']);
 Route::post('/pages/settingsPages/informationSettings', [HouseController::class, 'storeInformationSettings']);
