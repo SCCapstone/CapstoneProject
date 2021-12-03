aeaec78ab62068d014a1d2e90d9c4c904f116c69
Colin Bowes <59944164+OMHavec@users.noreply.github.com>
2021-12-02 20:45:16 -0500

    Update shopping.blade.php

--- resources/views/pages/shopping.blade.php
+++ resources/views/pages/shopping.blade.php
@@ -16,25 +16,25 @@
     <tbody>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox1" class="checkbox-form" value="{{ DB::table('shopping')->where('id',1)->value('done') }}">
                 </form>
             </td>
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item1" class="item-form" value="{{ DB::table('shopping')->where('id',1)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency1" class="urgency-form" value="{{ DB::table('shopping')->where('id',1)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee1" class="assignee-form" value="{{ DB::table('shopping')->where('id',1)->value('assignee') }}">
                 </form>
@@ -42,25 +42,25 @@
         </tr>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox2" class="checkbox-form" value="{{ DB::table('shopping')->where('id',2)->value('done') }}">
                 </form>
             </td>
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item2" class="item-form" value="{{ DB::table('shopping')->where('id',2)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency2" class="urgency-form" value="{{ DB::table('shopping')->where('id',2)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee2" class="assignee-form" value="{{ DB::table('shopping')->where('id',2)->value('assignee') }}">
                 </form>
@@ -68,25 +68,25 @@
         </tr>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox3" class="checkbox-form" value="{{ DB::table('shopping')->where('id',3)->value('done') }}">
                 </form>
             </td>            
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item3" class="item-form" value="{{ DB::table('shopping')->where('id',3)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency3" class="urgency-form" value="{{ DB::table('shopping')->where('id',3)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee3" class="assignee-form" value="{{ DB::table('shopping')->where('id',3)->value('assignee') }}">
                 </form>
@@ -94,25 +94,25 @@
         </tr>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox4" class="checkbox-form" value="{{ DB::table('shopping')->where('id',4)->value('done') }}">
                 </form>
             </td>
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item4" class="item-form" value="{{ DB::table('shopping')->where('id',4)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency4" class="urgency-form" value="{{ DB::table('shopping')->where('id',4)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee4" class="assignee-form" value="{{ DB::table('shopping')->where('id',4)->value('assignee') }}">
                 </form>
@@ -120,25 +120,25 @@
         </tr>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox5" class="checkbox-form" value="{{ DB::table('shopping')->where('id',5)->value('done') }}">
                 </form>
             </td>
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item5" class="item-form" value="{{ DB::table('shopping')->where('id',5)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency5" class="urgency-form" value="{{ DB::table('shopping')->where('id',5)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee5" class="assignee-form" value="{{ DB::table('shopping')->where('id',5)->value('assignee') }}">
                 </form>
@@ -146,25 +146,25 @@
         </tr>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox6" class="checkbox-form" value="{{ DB::table('shopping')->where('id',6)->value('done') }}">
                 </form>
             </td>
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item6" class="item-form" value="{{ DB::table('shopping')->where('id',6)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency6" class="urgency-form" value="{{ DB::table('shopping')->where('id',6)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee6" class="assignee-form" value="{{ DB::table('shopping')->where('id',6)->value('assignee') }}">
                 </form>
@@ -172,25 +172,25 @@
         </tr>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox7" class="checkbox-form" value="{{ DB::table('shopping')->where('id',7)->value('done') }}">
                 </form>
             </td>
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item7" class="item-form" value="{{ DB::table('shopping')->where('id',7)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency7" class="urgency-form" value="{{ DB::table('shopping')->where('id',7)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee7" class="assignee-form" value="{{ DB::table('shopping')->where('id',7)->value('assignee') }}">
                 </form>
@@ -198,25 +198,25 @@
         </tr>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox8" class="checkbox-form" value="{{ DB::table('shopping')->where('id',8)->value('done') }}">
                 </form>
             </td>
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item8" class="item-form" value="{{ DB::table('shopping')->where('id',8)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency8" class="urgency-form" value="{{ DB::table('shopping')->where('id',8)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee8" class="assignee-form" value="{{ DB::table('shopping')->where('id',8)->value('assignee') }}">
                 </form>
@@ -224,25 +224,25 @@
         </tr>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox9" class="checkbox-form" value="{{ DB::table('shopping')->where('id',9)->value('done') }}">
                 </form>
             </td>
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item9" class="item-form" value="{{ DB::table('shopping')->where('id',9)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency9" class="urgency-form" value="{{ DB::table('shopping')->where('id',9)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee9" class="assignee-form" value="{{ DB::table('shopping')->where('id',9)->value('assignee') }}">
                 </form>
@@ -250,25 +250,25 @@
         </tr>
         <tr>
             <td class="checkbox-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="checkbox" name="checkbox10" class="checkbox-form" value="{{ DB::table('shopping')->where('id',10)->value('done') }}">
                 </form>
             </td>
             <td class="item-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="item10" class="item-form" value="{{ DB::table('shopping')->where('id',10)->value('item') }}">
                 </form>
             </td>
             <td class="urgency-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="urgency10" class="urgency-form" value="{{ DB::table('shopping')->where('id',10)->value('urgency') }}">
                 </form>
             </td>
             <td class="assignee-column">
-                <form action="/shopping" method="POST">
+                <form action="/pages/shopping" method="POST">
                     @csrf
                     <input type="text" name="assignee10" class="assignee-form" value="{{ DB::table('shopping')->where('id',10)->value('assignee') }}">
                 </form>
