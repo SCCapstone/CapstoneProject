2716bfdd1d487d4c5420e3e548b25bd7dc815f75
Colin Bowes <59944164+OMHavec@users.noreply.github.com>
2021-12-03 14:59:07 -0500

    Removed sign.css from layout.blade.php; deleted app.css

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,14 +3,12 @@
     <head>
         <title>HouseMates</title>
         <meta charset="utf-8">
-        <link href="/css/app.css" type="text/css" rel="stylesheet">
         <link href="/css/contact.css" type="text/css" rel="stylesheet">
         <link href="/css/header.css" type="text/css" rel="stylesheet">
         <link href="/css/home.css" type="text/css" rel="stylesheet">
         <link href="/css/settings.css" type="text/css" rel="stylesheet">
         <link href="/css/settingsNav.css" type="text/css" rel="stylesheet">
         <link href="/css/shopping.css" type="text/css" rel="stylesheet">
-        <link href="/css/sign.css" type="text/css" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
