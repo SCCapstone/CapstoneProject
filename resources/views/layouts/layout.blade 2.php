87383660033365d45cd9789e3fe4742ceeb08639
ctreid <ctreid@email.sc.edu>
2021-12-03 22:34:51 -0500

    fixed css overwriting the nav bar

--- resources/views/layouts/layout.blade 2.php
+++ resources/views/layouts/layout.blade 2.php
@@ -10,7 +10,6 @@
         <link href="/css/calendar.css" type="text/css" rel="stylesheet">
 
         <script src="{{ asset('resources/js/reminders.js')}}"></script>
-        
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
