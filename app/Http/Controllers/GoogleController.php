8af71bb32cd0a891166ce78564fea5964f2ade0a
laurenelee99 <lel2@email.sc.edu>
2022-02-10 18:36:43 -0500

    Change to Google Controller

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -28,6 +28,32 @@ class GoogleController extends Controller
     public function handleGoogleCallback()
     {
         try {
+            $user = Socialite::driver('google')->user();
+        } catch (\Exception $e) {
+            return redirect('/login');
+        }
+        // only allow people with @company.com to login
+        if(explode("@", $user->email)[1] !== 'company.com'){
+            return redirect()->to('/');
+        }
+        // check if they're an existing user
+        $existingUser = User::where('email', $user->email)->first();
+        if($existingUser){
+            // log them in
+            auth()->login($existingUser, true);
+        } else {
+            // create a new user
+            $newUser                  = new User;
+            $newUser->name            = $user->name;
+            $newUser->email           = $user->email;
+            $newUser->google_id       = $user->id;
+            $newUser->avatar          = $user->avatar;
+            $newUser->avatar_original = $user->avatar_original;
+            $newUser->save();
+            auth()->login($newUser, true);
+        }
+        return redirect()->to('/home');
+        /* try {
       
             $user = Socialite::driver('google')->user();
        
@@ -54,6 +80,6 @@ class GoogleController extends Controller
       
         } catch (Exception $e) {
             dd($e->getMessage());
-        }
+        } */
     }
 }
