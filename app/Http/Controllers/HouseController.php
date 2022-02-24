4f170440317d7ad7d72831375d36ad9f1e9a6384
Nicolas-A-Becker <nico_becker1@aol.com>
2022-02-24 18:43:43 -0500

    changed to user_id in where call

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -44,7 +44,7 @@ class HouseController extends Controller
     }
     public function assignRoom(Request $req){
         $id = Auth::user()->id;
-        $affected = DB::update('UPDATE users SET house_num=? WHERE id=?', [request('roomnum'), $id]);
+        $affected = DB::update('UPDATE users SET house_num=? WHERE user_id=?', [request('roomnum'), $id]);
 
         return redirect()->intended('/pages/home-page');
     }
