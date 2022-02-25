6522296caf22064b0341c71b05de4496eab3f3b5
DaRosc <70486885+DaRosc@users.noreply.github.com>
2022-02-25 12:11:34 -0500

    Fixed user pointers

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -208,7 +208,7 @@ class HouseController extends Controller
 
     public function storePersonalSettings() {
         $updateDetails = [
-            'Name' => request('name'),
+            'name' => request('name'),
             'phone' => request('phone'),
             'email' => request('email'),
             'address' => request('address')
