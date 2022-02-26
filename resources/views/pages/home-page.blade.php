e5a04ba95210029dd81fdc39afc5a6216e18b3a4
laurenelee99 <lel2@email.sc.edu>
2022-02-26 11:43:56 -0600

    getting rid of house reminders

--- resources/views/pages/home-page.blade.php
+++ resources/views/pages/home-page.blade.php
@@ -7,12 +7,13 @@
 
 	<div id="box1"></div>
     	<div id="alert-rectangle"></div>
-    	<div id="alert2-rectangle"></div>
-    	<div style = "position:absolute; left:540px; top:255px; color:black; font-size:72px; text-decoration: underline; font-weight: bold; font-family: Roboto">
-	House Name
+		<!-- Getting rid of house reminders -->
+    	<!-- <div id="alert2-rectangle"></div> -->
+    	<!-- <div style = "position:absolute; left:540px; top:255px; color:black; font-size:72px; text-decoration: underline; font-weight: bold; font-family: Roboto">
+	House Name -->
 	<!-- Example session call <h2>{{session('email')}}</h2> -->
       	<!-- Change name based on user input -->
-    	</div>
+    	<!-- </div> -->
 
 	<div style = "position:absolute; left:40px; top:525px; color:black; font-size:48px; font-weight: bold; font-family: Roboto" value="{{ DB::table('users')->where('id', Auth::user()->id)->value('email') }}">
       	Alerts:
