069697c6c4334f66e1f1300e4284e8cad264db65
laurenelee99 <lel2@email.sc.edu>
2022-02-26 11:46:00 -0600

    get rid of house reminders, bring back house name

--- resources/views/pages/home-page.blade.php
+++ resources/views/pages/home-page.blade.php
@@ -9,20 +9,21 @@
     	<div id="alert-rectangle"></div>
 		<!-- Getting rid of house reminders -->
     	<!-- <div id="alert2-rectangle"></div> -->
-    	<!-- <div style = "position:absolute; left:540px; top:255px; color:black; font-size:72px; text-decoration: underline; font-weight: bold; font-family: Roboto">
-	House Name -->
+    	<div style = "position:absolute; left:540px; top:255px; color:black; font-size:72px; text-decoration: underline; font-weight: bold; font-family: Roboto">
+	House Name
 	<!-- Example session call <h2>{{session('email')}}</h2> -->
       	<!-- Change name based on user input -->
-    	<!-- </div> -->
+    	</div>
 
 	<div style = "position:absolute; left:40px; top:525px; color:black; font-size:48px; font-weight: bold; font-family: Roboto" value="{{ DB::table('users')->where('id', Auth::user()->id)->value('email') }}">
       	Alerts:
     	</div>
     	<!-- display alerts based on user input -->
 
-	<div style = "position:absolute; left:40px; top:725px; color:black; font-size:24px; font-weight: bold; font-family: Roboto">
+		<!-- getting rid of house reminders -->
+	<!-- <div style = "position:absolute; left:40px; top:725px; color:black; font-size:24px; font-weight: bold; font-family: Roboto">
       	House Reminders:
-    	</div>
+    	</div> -->
     	<!-- add vertical line -->
 </body>
 
