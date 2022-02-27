78acab00d86cefc48bb2d99445127c4f9949cdf0
DaRosc <70486885+DaRosc@users.noreply.github.com>
2022-02-27 09:59:49 -0500

    Implemented calendar migration

--- resources/views/pages/settingsPages/personalSettings.blade.php
+++ resources/views/pages/settingsPages/personalSettings.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.settings')
 @section('subSettingsPages')
 	
-	<div style="position:absolute; top:250px; left:345px;">
+	<div style="position:absolute; top:250px; left:350px;">
 		<form action='/pages/settingsPages/personalSettings' method="POST">
 			@csrf
 			<label for="name">Name</label><br>
 			<input size="50" type="text" id="name" name="name" value="{{ DB::table('users')->where('id', Auth::user()->id)->value('name') }}"><br>
 			<label for="phone">Phone Number</label><br>
-			<input size="50" type="text" id="phone" name="phone" value="{{ DB::table('users')->where('id', Auth::user()->id)->value('phone') }}" ><br>
+			<input size="50" type="text" id="phone" name="phone" value="{{ DB::table('users')->where('id', Auth::user()->id)->value('phone') }}"><br>
 			<label for="email">Email Address</label><br>
-			<input size="50" type="text" id="email" name="email" value="{{ DB::table('users')->where('id', Auth::user()->id)->value('email') }}" ><br>
+			<input size="50" type="text" id="email" name="email" value="{{ DB::table('users')->where('id', Auth::user()->id)->value('email') }}"><br>
 			<label for="address">Address</label><br>
 			<input size="50" type="text" id="address" name="address" value="{{ DB::table('users')->where('id', Auth::user()->id)->value('address') }}"><br>
+			<label for="calendar">Calendar</label><br>
+			<input size="50" type="text" id="calendar" name="calendar" value="{{ DB::table('users')->where('id', Auth::user()->id)->value('calendar') }}"><br>
 			<input type="submit" value="Publish">
 		</form>
 	</div>
