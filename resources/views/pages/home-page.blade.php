1cac611ddd135795a9c7c7d82eca1e6859316d81
laurenelee99 <lel2@email.sc.edu>
2022-02-27 20:22:58 -0600

    pushing to pull

--- resources/views/pages/home-page.blade.php
+++ resources/views/pages/home-page.blade.php
@@ -8,8 +8,9 @@
 	<div id="box1"></div>
     	<div id="alert-rectangle"></div>
     	<!-- <div id="alert2-rectangle"></div> -->
-    	<div style = "position:absolute; left:540px; top:255px; color:black; font-size:72px; text-decoration: underline; font-weight: bold; font-family: Roboto">
-		House Name
+    	<div style = "position:absolute; left:540px; top:255px; color:black; font-size:72px; text-decoration: underline; font-weight: bold; font-family: Roboto" value ="{{ DB::table('users')->where('id', Auth::user()->id)->value('house_num') }}">
+		<h1 class="home-housename"> House Name: {{ Auth::user()->house_num }}</h1>
+		<!-- House Name -->
 		<!-- Example session call <h2>{{session('email')}}</h2> -->
       	<!-- Change name based on user input -->
 		  <!-- TODO -->  
@@ -20,9 +21,7 @@
 	<div style = "position:absolute; left:40px; top:525px; color:black; font-size:48px; font-weight: bold; font-family: Roboto">
 		Alerts:
     </div>
-	<div style = "position: absolute; left: 50px; 525pxl color: black; font-size: 38px; font-family: Roboto" value ="{{ DB::table('chores-table')->where('id', Auth::user()->id)->value('name') }}">
-
-	</div>
+	<div style = "position: absolute; left: 55px; top: 525px; color: black; font-size: 38px; font-family: Roboto" value ="{{ DB::table('chores-table')->where('house_num', Auth::user()->house_num)->value('chores') }}"></div>
 		<!-- TODO -->
     	<!-- display alerts based on user input -->
 
