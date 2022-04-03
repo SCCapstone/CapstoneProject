4003f8339379e50f919eeccc42db6ec122926860
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2022-04-03 18:23:45 -0400

    adding instructions

--- resources/views/pages/calendar.blade.php
+++ resources/views/pages/calendar.blade.php
@@ -2,23 +2,20 @@
 @section('content')
 
 <div class="calendar">
-  <iframe id="google-calendar" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=Y3RyOTIyQGdtYWlsLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=dGhldG9waGVyaW5hdG9yQGdtYWlsLmNvbQ&color=%237986CB&color=%23F6BF26&color=%23EF6C00" frameborder="0" scrolling="no"></iframe> 
+  <iframe id="google-calendar" src={{ DB::table('users')->where('house-num', Auth::user()->house_num)->value('calendar') }} frameborder="0" scrolling="no"></iframe> 
   <!-- The link below is for the js file without using class
   <script src="reminders.js"></script>
   <script src="{{asset('resources/js/reminders.js')}}"></script> -->
 </div>
 
-<html>
-  <body>
-    <h1>If your calendar is displaying a 404 error, follow these steps:</h1>
-    <ol>
-      <li>Go to https://calendar.google.com/calendar/u/0/r/settings/calendar/Y3RyOTIyQGdtYWlsLmNvbQ?tab=rc</li>
-      <li>Scroll down to “Integrate Calendar” section</li>
-      <li>Copy the Embed code link</li>
-      <li>Go to the settings page (on Housemates.fun)</li>
-      <li>Paste the link in the Personal Profile page under Calendar</li>
-    </ol>
-  </body>
-</html>
-
+<div class="instructions">
+  <h3>If your calendar is displaying a 404 error, follow these steps:</h3>
+  <ol>
+    <li>Go to <a href="https://www.w3schools.com/html/default.asp" target="_blank">this page:</a></li>
+    <li>Scroll down to the “Integrate Calendar” section</li>
+    <li>Copy the Embed code link</li>
+    <li>Go to the Settings Page (on Housemates.fun)</li>
+    <li>Paste the link in the Personal Profile page under Calendar</li>
+  </ol>
+</div>
 @endsection
