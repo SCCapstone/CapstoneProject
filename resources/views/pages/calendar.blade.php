72bcc849ec7f4b9cb09d54dbc84889837ba6f426
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2022-04-03 18:47:26 -0400

    updated instructions

--- resources/views/pages/calendar.blade.php
+++ resources/views/pages/calendar.blade.php
@@ -12,8 +12,10 @@
   <h3>If your calendar is displaying a 404 error, follow these steps:</h3>
   <ol>
     <li>Go to <a href="https://calendar.google.com/calendar/u/0/r/settings/calendar/Y3RyOTIyQGdtYWlsLmNvbQ?tab=rc" target="_blank">this page:</a></li>
+    <li>Click on the name of the calendar you wish to integrate under "Settings for my calendars" on the left menu</li>
     <li>Scroll down to the “Integrate Calendar” section</li>
-    <li>Copy the Embed code link</li>
+    <li>Click on Customize and adjust your settings according to your preference</li>
+    <li>Copy the Embed code link at the top of the page</li>
     <li>Go to the Settings Page (on Housemates.fun)</li>
     <li>Paste the link in the Personal Profile page under Calendar</li>
   </ol>
